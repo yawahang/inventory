12737884cd71352bd4d0f21a994e6aca40bba8c4
Yawa Rai <yawa.rai@aqore.com>
2020-12-26 02:11:25 +0545

    moved controllers to controller folder, add custom exception handle stack trace

---
+++ inventory.web.api/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace inventory.api.Controllers
+{
+    [Produces("application/json")]
+    [EnableCors("AllowOrigin"), Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+
+    }
+}
